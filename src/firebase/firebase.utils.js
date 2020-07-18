6864179c402efc26695ee4bd3fca80b9733aaf23
Daniel <filind85@gmail.com>
2020-07-18 10:39:20 +0300

    added signin and signup and authentication with firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,53 @@
+import firebase from 'firebase/app';
+
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyB7R49MO1ru_EHiNgNaV6Pq4m5yA3ws3no",
+    authDomain: "clothes-shop-react.firebaseapp.com",
+    databaseURL: "https://clothes-shop-react.firebaseio.com",
+    projectId: "clothes-shop-react",
+    storageBucket: "clothes-shop-react.appspot.com",
+    messagingSenderId: "913354345658",
+    appId: "1:913354345658:web:55b2e1b3ba22b3a6e3c683",
+    measurementId: "G-2E8F334RVZ"
+  };
+
+export const createUserProfileDocument = async (userAuth, addintionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName, 
+        email,
+        createdAt,
+        ...addintionalData
+      })
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return userRef;
+} 
+
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ propmt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
+
